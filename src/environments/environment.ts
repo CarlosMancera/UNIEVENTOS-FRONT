5db7786e86e7d2cbbc5404b379a99b3de31f5c84
ndortiz <ndortiz@bancolombia.com.co>
2025-04-08 22:38:30 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  // urlAuth: 'https://unieventosbackend-production.up.railway.app/api/auth',
-  // urlAuthAux: 'https://unieventosbackend-production.up.railway.app/api/cuenta',
-  urlAuth: 'http://localhost:8081/api/auth',
-  urlAuthAux: 'http://localhost:8081/api/cuenta',
-  urlBase:'http://localhost:8081',
+   urlAuth: 'https://unieventosbackend-production.up.railway.app/api/auth',
+   urlAuthAux: 'https://unieventosbackend-production.up.railway.app/api/cuenta',
+   urlBase: 'https://unieventosbackend-production.up.railway.app'
+  //urlAuth: 'http://localhost:8081/api/auth',
+  //urlAuthAux: 'http://localhost:8081/api/cuenta',
+  //urlBase:'http://localhost:8081',
 };
