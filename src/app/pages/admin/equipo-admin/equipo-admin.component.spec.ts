11a3708be04cb29038b7e5e543145bf8096a892e
Carlos Mancera <carlosh.mancerar@uqvirtual.edu.co>
2025-05-24 18:57:16 -0500

    Ajustes en tests

--- src/app/pages/admin/equipo-admin/equipo-admin.component.spec.ts
+++ src/app/pages/admin/equipo-admin/equipo-admin.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EquipoAdminComponent } from './equipo-admin.component';
-import { TeamService } from '../../../services/Team.service';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { TeamService } from '../../../services/Team.service';
 import { BcLoadingService } from '../../../services/loading.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
+import { Team } from '../../../models/team.model';
 
 // Mocks
 class MockTeamService {
@@ -18,7 +21,7 @@ class MockMatDialog {
   });
 }
 
-class MockSnackBar {
+class MockMatSnackBar {
   open = jasmine.createSpy();
 }
 
@@ -33,11 +36,15 @@ describe('EquipoAdminComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EquipoAdminComponent],
+      imports: [
+        EquipoAdminComponent,
+        HttpClientTestingModule,
+        BrowserAnimationsModule
+      ],
       providers: [
         { provide: TeamService, useClass: MockTeamService },
         { provide: MatDialog, useClass: MockMatDialog },
-        { provide: MatSnackBar, useClass: MockSnackBar },
+        { provide: MatSnackBar, useClass: MockMatSnackBar },
         { provide: BcLoadingService, useClass: MockBcLoadingService }
       ]
     }).compileComponents();
@@ -51,11 +58,11 @@ describe('EquipoAdminComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call listar equipos on init', () => {
+  it('should call teamService.listar on init', () => {
     expect(component['teamService'].listar).toHaveBeenCalled();
   });
 
-  it('should open dialog and reload equipos after modal closed', () => {
+  it('should call cargarEquipos and show snackbar after modal closes', () => {
     const spy = spyOn(component, 'cargarEquipos');
     component.abrirModal();
     expect(component['dialog'].open).toHaveBeenCalled();
@@ -63,7 +70,7 @@ describe('EquipoAdminComponent', () => {
     expect(component['snackBar'].open).toHaveBeenCalledWith('Equipo guardado correctamente', 'Cerrar', { duration: 3000 });
   });
 
-  it('should call eliminar equipo and reload equipos', () => {
+  it('should call eliminar, recargar y mostrar snackbar', () => {
     const spy = spyOn(component, 'cargarEquipos');
     component.eliminar(1);
     expect(component['teamService'].eliminar).toHaveBeenCalledWith(1);
--- src/app/shared/modals/modal-equipo/modal-equipo.component.spec.ts
+++ src/app/shared/modals/modal-equipo/modal-equipo.component.spec.ts
@@ -2,9 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ModalEquipoComponent } from './modal-equipo.component';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
 
-// Servicios personalizados mockeados
+import { TeamService } from '../../../services/Team.service';
+import { ImagenService } from '../../../services/ImagenService.service';
+import { BcLoadingService } from '../../../services/loading.service';
+
+// Mocks
 class MockTeamService {
   crear = jasmine.createSpy().and.returnValue(of({}));
   actualizar = jasmine.createSpy().and.returnValue(of({}));
@@ -25,13 +30,17 @@ describe('ModalEquipoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ModalEquipoComponent, HttpClientTestingModule],
+      imports: [
+        ModalEquipoComponent,
+        HttpClientTestingModule,
+        BrowserAnimationsModule // <- para evitar errores de animaciones
+      ],
       providers: [
         { provide: MatDialogRef, useValue: { close: jasmine.createSpy() } },
         { provide: MAT_DIALOG_DATA, useValue: null },
-        { provide: 'TeamService', useClass: MockTeamService },
-        { provide: 'ImagenService', useClass: MockImagenService },
-        { provide: 'BcLoadingService', useClass: MockBcLoadingService },
+        { provide: TeamService, useClass: MockTeamService },
+        { provide: ImagenService, useClass: MockImagenService },
+        { provide: BcLoadingService, useClass: MockBcLoadingService }
       ]
     }).compileComponents();
 
