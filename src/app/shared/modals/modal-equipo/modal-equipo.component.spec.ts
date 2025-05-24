f190547e7ff0cd8eca9490ddf3cd2db5cc67176e
Carlos Mancera <carlosh.mancerar@uqvirtual.edu.co>
2025-05-24 18:32:28 -0500

    Update modal-equipo.component.spec.ts

--- src/app/shared/modals/modal-equipo/modal-equipo.component.spec.ts
+++ src/app/shared/modals/modal-equipo/modal-equipo.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ModalEquipoComponent } from './modal-equipo.component';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+
+// Servicios personalizados mockeados
+class MockTeamService {
+  crear = jasmine.createSpy().and.returnValue(of({}));
+  actualizar = jasmine.createSpy().and.returnValue(of({}));
+}
+
+class MockImagenService {
+  subirImagen = jasmine.createSpy().and.returnValue(of('url-de-prueba'));
+}
+
+class MockBcLoadingService {
+  show = jasmine.createSpy();
+  close = jasmine.createSpy();
+}
 
 describe('ModalEquipoComponent', () => {
   let component: ModalEquipoComponent;
@@ -8,9 +25,15 @@ describe('ModalEquipoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ModalEquipoComponent]
-    })
-    .compileComponents();
+      imports: [ModalEquipoComponent, HttpClientTestingModule],
+      providers: [
+        { provide: MatDialogRef, useValue: { close: jasmine.createSpy() } },
+        { provide: MAT_DIALOG_DATA, useValue: null },
+        { provide: 'TeamService', useClass: MockTeamService },
+        { provide: 'ImagenService', useClass: MockImagenService },
+        { provide: 'BcLoadingService', useClass: MockBcLoadingService },
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ModalEquipoComponent);
     component = fixture.componentInstance;
