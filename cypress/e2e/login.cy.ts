5def508f57c07f1c1d4587d5156c27024a000d0d
Carlos Mancera <carlosh.mancerar@uqvirtual.edu.co>
2025-05-26 10:05:49 -0500

    Update login.cy.ts

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -2,7 +2,7 @@ describe('C101 - Login exitoso', () => {
   it('C101_should_login_successfully', () => {
     cy.visit('/login');
 
-    cy.get('input[name="email"]').type('nicolas95051195@gmail.com');
+    cy.get('input[name="email"]').type('ndortizp@uqvirtual.edu.co');
     cy.get('input[name="password"]').type('1234');
 
     cy.get('button.login-button').click();
