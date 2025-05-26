1fa93cff67e41ae5d52dcc5ba2a8b125b242152b
Carlos Mancera <carlosh.mancerar@uqvirtual.edu.co>
2025-05-26 08:41:18 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  urlAuth: 'https://unieventosbackend-production-4a3d.up.railway.app/api/auth',
-  urlAuthAux: 'https://unieventosbackend-production-4a3d.up.railway.app/api/cuenta',
-  urlBase: 'https://unieventosbackend-production-4a3d.up.railway.app'
+  urlAuth: 'https://unieventosbackend-production-2f20.up.railway.app/api/auth',
+  urlAuthAux: 'https://unieventosbackend-production-2f20.up.railway.app/api/cuenta',
+  urlBase: 'https://unieventosbackend-production-2f20.up.railway.app'
   // urlAuth: 'http://localhost:8081/api/auth',
   // urlAuthAux: 'http://localhost:8081/api/cuenta',
   // urlBase:'http://localhost:8081',
