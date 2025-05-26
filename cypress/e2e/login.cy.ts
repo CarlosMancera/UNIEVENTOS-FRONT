7a75fa1aba0963c713c0965ff21a180df55f347f
Carlos Mancera <carlosh.mancerar@uqvirtual.edu.co>
2025-05-26 11:52:25 -0500

    Ajuste de test e2e

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,12 +1,17 @@
 describe('C1 - Login exitoso', () => {
-  it('C1_should_login_successfully', () => {
+  it('C1_should_login_successfully_and_logout', () => {
     cy.visit('/login');
 
     cy.get('input[name="email"]').type('ndortizp@uqvirtual.edu.co');
     cy.get('input[name="password"]').type('1234');
-
     cy.get('button.login-button').click();
 
     cy.contains('¡Bienvenido!').should('exist');
+
+    cy.get('.user-icon.dropdown').should('contain.text', 'Admin Ortiz P').click();
+    cy.contains('Cerrar sesión').click();
+
+    cy.url().should('include', '/');
+    cy.get('.user-icon a[routerlink="/login"]').should('exist');
   });
 });
